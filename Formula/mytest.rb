7f8195a5707ff3635f48beb5b812b66865bf0228
vsts <vsts@Mac-445.local>
2019-04-16 15:39:08 +0000

    mytest: add 0.1.1 bottle.

--- Formula/mytest.rb
+++ Formula/mytest.rb
@@ -1,5 +1,12 @@
 class Mytest < Formula
     desc "Test formula"
+  bottle do
+    root_url "https://dl.bintray.com/rtimush/bottles-test-tap"
+    cellar :any_skip_relocation
+    sha256 "ebf7aef04f3463cc09b6bb85fdb8c11c919ef234953984da4cd8acd14a3728da" => :mojave
+    sha256 "ef97611b7ae2736b5ddfc1d44f8003f9a7bf4761bb4528d2fb96e11434ce534a" => :high_sierra
+  end
+
     homepage "https://github.com/rtimush/tenpureto"
 
     url "https://github.com/rtimush/tenpureto/archive/v0.1.1.tar.gz"
