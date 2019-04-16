5fd6a45530a9e0ad27ebb5a8cab7703fea07cf1a
Roman Timushev <rtimush@gmail.com>
2019-04-16 19:47:28 +0200

    Update revision to 3

--- Formula/mytest.rb
+++ Formula/mytest.rb
@@ -1,10 +1,10 @@
 class Mytest < Formula
-  desc "Test formula 1"
+  desc "Test formula"
   homepage "https://github.com/rtimush/tenpureto"
 
   url "https://github.com/rtimush/tenpureto/archive/v0.1.1.tar.gz"
   sha256 "e18349cf8db2293b453a091ea30755b5d7996b09baadb7a57ca0253e302d95eb"
-  revision 2
+  revision 3
 
   bottle do
     root_url "https://dl.bintray.com/rtimush/bottles-test-tap"
