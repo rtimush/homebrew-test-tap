6c95c65031c89a469d4a734bd79c108d6167dd63
Roman Timushev <831307+rtimush@users.noreply.github.com>
2019-07-28 01:51:17 +0200

    Build on travis

--- Formula/mytest.rb
+++ Formula/mytest.rb
@@ -2,9 +2,9 @@ class Mytest < Formula
   desc "Test formula"
   homepage "https://github.com/rtimush/tenpureto"
 
-  url "https://github.com/rtimush/tenpureto/archive/v0.1.1.tar.gz"
-  sha256 "e18349cf8db2293b453a091ea30755b5d7996b09baadb7a57ca0253e302d95eb"
-  revision 6
+  url "https://github.com/rtimush/tenpureto/archive/v0.1.2.tar.gz"
+  sha256 "6c2cdf03c88cc012fb75a5c937fabd3b739cecccbd99e883375516e8dea2ea12"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/rtimush/bottles-test-tap"
--- bot.sh
+++ bot.sh
@@ -4,20 +4,20 @@ set -euxo pipefail
 
 #
 # Required environment variables:
-#   BUILD_REPOSITORY_ID                  - org/repo
-#   SYSTEM_PULLREQUEST_PULLREQUESTNUMBER - pull request number, defined for PR builds only
-#   SYSTEM_PULLREQUEST_TARGETBRANCH      - pull request target branch, defined for PR builds only
-#   BUILD_ARTIFACTSTAGINGDIRECTORY       - directory to copy bottles to
+#   TRAVIS_REPO_SLUG                     - org/repo
+#   TRAVIS_PULL_REQUEST                  - pull request number, defined for PR builds only
+#   TRAVIS_BRANCH                        - pull request target branch, defined for PR builds only
+#   TRAVIS_BUILD_ID                      - used as a temporary bottles key
 #   BINTRAY_ORG (optional)               - bintray organization name, defaults to git organization name
 #   BINTRAY_REPO (optional)              - bintray repository name, defaults to bottles-${TAP_SHORT_NAME}
 #   BINTRAY_USER (optional)              - bintray user name, defaults to BINTRAY_ORG
 #   BINTRAY_KEY                          - bintray API key
-#   GITHUB_DEPLOY_KEY                    - SSH key with (\n can represent a newline, as Azure Pipelines do not support multiline variables)
+#   GITHUB_DEPLOY_KEY                    - SSH key
 #
 
 setupEnvironment() {
-    if [[ -n "${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER:-}" ]]; then
-        export GIT_PREVIOUS_COMMIT=$(git rev-parse --verify "origin/$SYSTEM_PULLREQUEST_TARGETBRANCH")
+    if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+        export GIT_PREVIOUS_COMMIT=$(git rev-parse --verify "origin/$TRAVIS_BRANCH")
         export GIT_COMMIT=$(git rev-parse --verify "HEAD^2")
         CI_MODE=--ci-pr
     else
@@ -26,56 +26,65 @@ setupEnvironment() {
         CI_MODE=--ci-testing
     fi
     export HOMEBREW_NO_ANALYTICS=1
-    export HOMEBREW_NO_AUTO_UPDATE=1
-    REPO_NAME="${BUILD_REPOSITORY_ID#*/}"
-    ORG_NAME="${BUILD_REPOSITORY_ID%/*}"
+    REPO_NAME="${TRAVIS_REPO_SLUG#*/}"
+    ORG_NAME="${TRAVIS_REPO_SLUG%/*}"
     TAP_SHORT_NAME="${REPO_NAME#homebrew-}"
     TAP_NAME="$ORG_NAME/$TAP_SHORT_NAME"
     BINTRAY_ORG="${BINTRAY_ORG:-${ORG_NAME}}"
     BINTRAY_REPO="${BINTRAY_REPO:-bottles-${TAP_SHORT_NAME}}"
     BINTRAY_USER="${BINTRAY_USER:-${BINTRAY_ORG}}"
-    unset TF_BUILD
+    TEMP_BOTTLE_PACKAGE="tmp"
+    TEMP_BOTTLE_VERSION="${TRAVIS_BUILD_ID}"
+    unset TRAVIS
+    export JFROG_CLI_OFFER_CONFIG=false
+    # begin https://github.com/Homebrew/brew/issues/5561 workaround
+    rm -rf /usr/local/Homebrew/Library/Homebrew/vendor/bundle/ruby/2.3.0/
+    brew vendor-install ruby
+    # end workaround
 }
 
 setupTap() {
-    local tap="$(brew --repo)/Library/Taps/$BUILD_REPOSITORY_ID"
+    local tap="$(brew --repo)/Library/Taps/$TRAVIS_REPO_SLUG"
     mkdir -p "$(dirname "$tap")"
     sudo ln -s "$PWD" "$tap"
 }
 
-BINTRAY_PUBLISH_URLS=()
+uploadTempBottle() {
+    local BOTTLE_JSON="$1"
+    local BOTTLE_LOCAL_FILENAME="$(jq -r '.[].bottle.tags[].local_filename' "$BOTTLE_JSON")"
+    jfrog bt upload --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" "${BOTTLE_JSON}" "${BINTRAY_ORG}/${BINTRAY_REPO}/${TEMP_BOTTLE_PACKAGE}/${TEMP_BOTTLE_VERSION}"
+    jfrog bt upload --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" "${BOTTLE_LOCAL_FILENAME}" "${BINTRAY_ORG}/${BINTRAY_REPO}/${TEMP_BOTTLE_PACKAGE}/${TEMP_BOTTLE_VERSION}"
+}
+
+downloadTempBottles() {
+    jfrog bt download-ver --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" --unpublished "${BINTRAY_ORG}/${BINTRAY_REPO}/${TEMP_BOTTLE_PACKAGE}/${TEMP_BOTTLE_VERSION}"
+    jfrog bt version-delete --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" --quiet=true "${BINTRAY_ORG}/${BINTRAY_REPO}/${TEMP_BOTTLE_PACKAGE}/${TEMP_BOTTLE_VERSION}"
+}
+
+BINTRAY_PUBLISH_PACKAGES=()
 
 uploadBottle() {
     local BOTTLE_JSON="$1"
     local BOTTLE_LOCAL_FILENAME="$(jq -r '.[].bottle.tags[].local_filename' "$BOTTLE_JSON")"
     local BOTTLE_FILENAME="$(jq -r '.[].bottle.tags[].filename' "$BOTTLE_JSON")"
-    local BOTTLE_REPOSITORY="$(jq -r '.[].bintray.repository' "$BOTTLE_JSON")"
     local BOTTLE_PACKAGE="$(jq -r '.[].bintray.package' "$BOTTLE_JSON")"
     local BOTTLE_VERSION="$(jq -r '.[].formula.pkg_version' "$BOTTLE_JSON")"
-    [[ $(curl -T "${BOTTLE_LOCAL_FILENAME}" -u "${BINTRAY_USER}:${BINTRAY_KEY}" -o /dev/stderr  -s -w "%{http_code}" \
-              "https://api.bintray.com/content/${BINTRAY_ORG}/${BOTTLE_REPOSITORY}/${BOTTLE_PACKAGE}/${BOTTLE_VERSION}/${BOTTLE_FILENAME}") \
-              -eq 201 ]] || return 1
-    BINTRAY_PUBLISH_URLS+=("https://api.bintray.com/content/${BINTRAY_ORG}/${BOTTLE_REPOSITORY}/${BOTTLE_PACKAGE}/${BOTTLE_VERSION}/publish")
+    jfrog bt upload --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" "${BOTTLE_LOCAL_FILENAME}" "${BINTRAY_ORG}/${BINTRAY_REPO}/${BOTTLE_PACKAGE}/${BOTTLE_VERSION}"
+    BINTRAY_PUBLISH_PACKAGES+=("${BINTRAY_ORG}/${BINTRAY_REPO}/${BOTTLE_PACKAGE}/${BOTTLE_VERSION}")
 }
 
 publishBottles() {
-    local url
-    for url in "${BINTRAY_PUBLISH_URLS[@]}"; do
-        curl -X POST -u "${BINTRAY_USER}:${BINTRAY_KEY}" -s "$url"
+    local pkg
+    for pkg in "${BINTRAY_PUBLISH_PACKAGES[@]}"; do
+        jfrog bt version-publish --user "${BINTRAY_USER}" --key "${BINTRAY_KEY}" "${BOTTLE_LOCAL_FILENAME}" "$pkg"
     done
 }
 
 setupGit() {
-    (
-        set +x
-        echo $GITHUB_DEPLOY_KEY | sed 's/\\n/\
-/g' > github.key
-    )
-    chmod 0600 github.key
     mkdir -p ~/.ssh
     ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-    git remote add origin-writeable "git@github.com:$BUILD_REPOSITORY_ID.git"
-    git config core.sshCommand "ssh -i github.key"
+    git remote add origin-writeable "git@github.com:$TRAVIS_REPO_SLUG.git"
+    git config core.sshCommand "ssh -i ${GITHUB_DEPLOY_KEY}"
     git config user.name "BrewTestBot"
     git config user.email "homebrew-test-bot@lists.sfconservancy.org"
 }
@@ -84,22 +93,28 @@ setupEnvironment
 setupTap
 
 case "$1" in
-    test)
-        sudo xcode-select --switch /Applications/Xcode_10.1.app/Contents/Developer
+    build)
+        brew update
         brew test-bot --bintray-org="$BINTRAY_ORG" \
-            --root-url=https://dl.bintray.com/$BINTRAY_ORG/$BINTRAY_REPO \
-            --tap=rtimush/test-tap \
+            --root-url="https://dl.bintray.com/$BINTRAY_ORG/$BINTRAY_REPO" \
+            --tap="$TAP_NAME" \
             --verbose \
             --no-pull \
             --junit \
             $CI_MODE
-
-        if compgen -G "*.bottle.*" > /dev/null; then
-            mv *.bottle.* "$BUILD_ARTIFACTSTAGINGDIRECTORY/"
+        brew install jfrog-cli-go jq
+        if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+            if compgen -G "*.bottle.*" > /dev/null; then
+                for f in *.bottle.json; do
+                    uploadTempBottle "$f"
+                done
+            fi
         fi
         ;;
 
     upload)
+        brew install jfrog-cli-go jq
+        downloadTempBottles
         if compgen -G "*.bottle.*" > /dev/null; then
             setupGit
             brew bottle --write --merge *.bottle.json
@@ -107,7 +122,8 @@ case "$1" in
                 uploadBottle "$f"
             done
             git merge origin/master -m "Merge updated bottles"
-            git push --atomic origin-writeable HEAD:master HEAD:staging && publishBottles
+            git push --atomic origin-writeable HEAD:master HEAD:staging
+            publishBottles
         fi
         ;;
 
