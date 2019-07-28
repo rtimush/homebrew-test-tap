1e78f69de2cfa00e8e47d82fa01f19cfa0fe93e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-28 19:11:00 +0000

    mytest: update 0.1.2_2 bottle.

--- Formula/mytest.rb
+++ Formula/mytest.rb
@@ -9,9 +9,9 @@ class Mytest < Formula
   bottle do
     root_url "https://dl.bintray.com/rtimush/bottles-test-tap"
     cellar :any_skip_relocation
-    sha256 "7b6c0e0602a04c2c6a79c89135b0dda3525e59669430406295596fe26d16631f" => :mojave
-    sha256 "f5779981d2c7d515399831f26cc0dfc53f70ac7c6a17a4adeaa5fd05f0863556" => :high_sierra
-    sha256 "7f7f904abdde26401c7043b8436009f3ccab2d90bfbe64f79261b8ea4a310de8" => :sierra
+    sha256 "6401614803ea9a1dd2f46c6a33281a335a9a8b06454ef3628e452e3dc9646d7d" => :mojave
+    sha256 "2cc9836da58c5e3fd5476d95873874d0afeecb6c8e7671de26a9910bde49ccc3" => :high_sierra
+    sha256 "047b14721771d8666eacee9bb6ec3e73785f15d4b3cd114fb2d0a3c980a33f11" => :sierra
   end
 
   def install
